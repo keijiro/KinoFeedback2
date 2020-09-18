83c138447bf59191c3ea8f7c4c073932c9e957e2
Keijiro Takahashi <keijiro@gmail.com>
2020-09-18 14:36:39 +0900

    Move to package directory

---
+++ Packages/jp.keijiro.kino.feedback/Editor/FeedbackEffectControllerEditor.cs
@@ -0,0 +1,38 @@
+using UnityEditor;
+
+namespace Kino {
+
+[CanEditMultipleObjects, CustomEditor(typeof(FeedbackEffectController))]
+sealed class FeedbackEffectControllerEditor : Editor
+{
+    SerializedProperty _tint;
+    SerializedProperty _offsetX;
+    SerializedProperty _offsetY;
+    SerializedProperty _rotation;
+    SerializedProperty _scale;
+    SerializedProperty _filter;
+
+    void OnEnable()
+    {
+        _tint = serializedObject.FindProperty("_tint");
+        _offsetX = serializedObject.FindProperty("_offsetX");
+        _offsetY = serializedObject.FindProperty("_offsetY");
+        _rotation = serializedObject.FindProperty("_rotation");
+        _scale = serializedObject.FindProperty("_scale");
+        _filter = serializedObject.FindProperty("_filter");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(_tint);
+        EditorGUILayout.PropertyField(_offsetX);
+        EditorGUILayout.PropertyField(_offsetY);
+        EditorGUILayout.PropertyField(_rotation);
+        EditorGUILayout.PropertyField(_scale);
+        EditorGUILayout.PropertyField(_filter);
+        serializedObject.ApplyModifiedProperties();
+    }
+}
+
+} // namespace Kino
